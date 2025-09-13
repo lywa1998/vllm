a5c04af8ebadc24c2029b044043c547c8b72f2ae
Shane A <shanea@allenai.org>
2025-09-12 20:26:21 -0700

    [Model] Add Olmo3 model implementation (#24534)
    Signed-off-by: Shane A <shanea@allenai.org>
    Co-authored-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- vllm/transformers_utils/configs/__init__.py
+++ vllm/transformers_utils/configs/__init__.py
@@ -23,6 +23,7 @@ from vllm.transformers_utils.configs.moonvit import MoonViTConfig
 from vllm.transformers_utils.configs.nemotron import NemotronConfig
 from vllm.transformers_utils.configs.nemotron_h import NemotronHConfig
 from vllm.transformers_utils.configs.nemotron_vl import Nemotron_Nano_VL_Config
+from vllm.transformers_utils.configs.olmo3 import Olmo3Config
 from vllm.transformers_utils.configs.ovis import OvisConfig
 from vllm.transformers_utils.configs.qwen2_5_omni_thinker import Qwen2_5OmniThinkerConfig
 from vllm.transformers_utils.configs.qwen3_next import Qwen3NextConfig
@@ -46,6 +47,7 @@ __all__ = [
     "NemotronConfig",
     "NemotronHConfig",
     "Nemotron_Nano_VL_Config",
+    "Olmo3Config",
     "OvisConfig",
     "SpeculatorsConfig",
     "Qwen2_5OmniThinkerConfig",
